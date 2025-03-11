b3049914db845629a2affe591bdd45ca4c0cbd04
emarishkin <ee.marishkin@yandex.ru>
2025-03-11 17:57:35 +0400

    3

--- TypeScript6/src/components/EventExample.tsx
+++ TypeScript6/src/components/EventExample.tsx
@@ -4,20 +4,21 @@ import { FC, useState } from "react"
 const EventExample:FC = () => {
 
     const [value,setValue] = useState<string>('')
+    const [click,setClick] = useState<boolean>(false)
 
 const ChangeHandler = (e:React.ChangeEvent<HTMLInputElement>) => {
     return setValue(e.target.value)
 }
 
 const HandleClick = (e:React.MouseEvent<HTMLButtonElement>) => {
-    return console.log(value)
-}
+    return setClick(prev=>!prev)
 
-    
+}
     return (
         <>
         <input value={value} onChange={ChangeHandler}  type="text" />
         <button onClick={HandleClick}>click</button>
+        <p>{click && value}</p>
         </>
     )
 }
