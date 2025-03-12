6e0446eb48b43a428fa6d99a09fce6041ded9cd2
emarishkin <ee.marishkin@yandex.ru>
2025-03-12 21:41:19 +0400

    7.1

--- TypeScript6/src/components/UserList.tsx
+++ TypeScript6/src/components/UserList.tsx
@@ -12,12 +12,13 @@ const [glaz,setGlaz] = useState<string>("password")
 
     return (
         <div>
+            <h1>Пользователи</h1>
+            <button onClick={()=>setGlaz('text')}>Посмотреть пароли</button>
           {users.map((user)=>
           <div style={{border:'2px solid black'}}>
            <h1>{user.id}. {user.username}</h1>
            <p>Логин: {user.email}</p>
            <input type={glaz} value={user.password} />
-           <button onMouseDown={()=>setGlaz('text')}>Глазик</button>
            <div>
             <h4>Персональные данные пользователя:</h4>
             <p>ФИО: {user.name.firstname } {user.name.lastname}</p>
